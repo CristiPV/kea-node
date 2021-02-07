3a04843457e6b90ef2cd1cfac8e30d2334037be6
Cristian <cristi.valentin29@gmail.com>
2021-02-07 11:33:35 +0100

    Add Objects exercises

--- 1._Intro_Exercises/3.Objects.js
+++ 1._Intro_Exercises/3.Objects.js
@@ -7,11 +7,21 @@ const myObj = {"message": "Hello, earthling! I bring peace."};
 
 // Log the message 
 
+const value = myObj.message;
+
+console.log("Retrieved value: " + value);
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const me = {
+    name: "Bob",
+    age: "9"
+};
+
+console.log(me);
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -20,6 +30,10 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
+console.log(stackOverflow);
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,6 +41,10 @@ const thisSong = {"description": "The best song in the world."}
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute";
+
+console.log(thisSong);
 
 // --------------------------------------
 
