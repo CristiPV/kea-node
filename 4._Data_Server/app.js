4831ce202eab4b8f04be50a56eb6898b66167a57
Cristian <cristi.valentin29@gmail.com>
2021-03-11 08:34:01 +0100

    Add json to Data Server

--- 4._Data_Server/app.js
+++ 4._Data_Server/app.js
@@ -1,6 +1,10 @@
 const express = require( "express" );
 const app = express();
 
+const cat = require( "./cat.json" );
+
+console.log( cat );
+
 app.use(express.json());
 
 app.get( "/querystring", ( req, res ) => {
@@ -14,9 +18,18 @@ app.get( "/pathvariable/:title/:message", ( req, res ) => {
     } );
 } );
 
+app.get( "/cat", ( req, res ) => {
+    res.send( cat );
+} );
+
 app.post( "/whatever", ( req, res ) => {
     // Get what is in the body and send it back
     res.send( req.body );
 } );
 
-app.listen( "3000" );
+app.listen( "8080", ( error ) => { 
+    if ( error ) {
+        console.log( error );
+    }
+    console.log( "Server is running on port ", 8080 );
+ } );
