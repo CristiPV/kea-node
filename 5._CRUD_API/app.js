872bb616b2d484a6460d9637c5c8113e927cbfbc
Cristian <cristi.valentin29@gmail.com>
2021-03-11 08:34:23 +0100

    Add error checking for CRUD API

--- 5._CRUD_API/app.js
+++ 5._CRUD_API/app.js
@@ -69,7 +69,12 @@ app.delete( "/students/:id", ( req, res ) => {
     }
 } );
 
-app.listen( "8080" );
+app.listen( "8080", ( error ) => { 
+    if ( error ) {
+        console.log( error );
+    }
+    console.log( "Server is running on port ", 8080 );
+ } );
 
 const studentList = [
     {
