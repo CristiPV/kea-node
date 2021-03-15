309a853c2a72769e224277c02407de73339455d8
Cristian <cristi.valentin29@gmail.com>
2021-03-15 09:46:19 +0100

    Update Carnivorous Plants API

--- 6._Carnivorous_Plants_CRUD_API/app.js
+++ 6._Carnivorous_Plants_CRUD_API/app.js
@@ -1,7 +1,21 @@
 const express = require( "express" );
 const app = express();
 
-const plants = require( "./carnivorous_plants.json" );
+const PORT = process.env.PORT || 8080;
+
+//const plants = require( "./carnivorous_plants.json" );
+
+let plants = [
+    {
+        "id": 1,
+        "name": "Cephalotus follicularis"
+    },
+    {
+        "id": 2,
+        "name": "Sarracenia purpurea"
+    }
+];
+let ID = plants.length;
 
 app.use( express.json() );
 
@@ -12,20 +26,50 @@ app.get( "/plants", ( req, res ) => {
 app.get( "/plants/:id", ( req, res ) => {
     const id = parseInt( req.params.id );
     
-    if ( id >= 0 && id < plants.length ) {
-        res.send( plants.find( ( plant ) => {
-            return plant.id === id;
-        } ) );
-    } else {
-        res.send( {
-            exception: "No carnivorous plant with the given id was found"
-        } );   
-    }
+    const foundPlant = plants.find( ( plant ) => {
+        return plant.id === id;
+    } );
+
+    const response = foundPlant ? foundPlant : {
+        exception: "No carnivorous plant with the given id was found"
+    };
+
+    res.send( response );
 } );
 
-app.listen( "8080", ( error ) => {
+app.post( "/plants", ( req, res ) => {
+    const newPlant = req.body;
+    newPlant.id = ++ID;
+    plants.push( newPlant );
+    res.send( newPlant );
+} );
+
+app.patch( "/plants/:id", ( req, res ) => {
+    const id = parseInt( req.params.id );
+    let hasUpdated = false
+
+    plants = plants.map( plant => { 
+        if ( plant.id === id ) {
+            hasUpdated = true;
+            return { ...plant, ...req.body, id: plant.id };
+        }
+        return plant;
+    } );
+
+    res.send( { hasUpdated: hasUpdated } );
+} );
+
+app.delete( "/plants/:id", ( req, res ) => {
+    const id = parseInt( req.params.id );
+
+    plants = plants.filter( plant => plant.id !== id );
+
+    res.send( { } );
+} );
+
+app.listen( PORT, ( error ) => {
     if ( error ) {
         console.log( error );
     }
-    console.log( "Server running on port: ", 8080 );
+    console.log( "Server running on port: ", PORT );
 } );
