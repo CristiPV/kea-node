fa71aad8c7dda10f04dde62b703cb316b8cd1aaf
Cristian <cristi.valentin29@gmail.com>
2021-03-15 09:46:45 +0100

    Update Express Wide Web API

--- 7._Express_Wide_Web/app.js
+++ 7._Express_Wide_Web/app.js
@@ -1,5 +1,6 @@
 const express = require( "express" );
 const app = express();
+const PORT = process.env.PORT ? process.env.PORT : 8080;
 
 const welcomePath = __dirname + "/public/welcome.html";
 const folderPath = __dirname + "/public";
@@ -13,9 +14,11 @@ app.get( "/dragons", ( req, res ) => {
     res.sendFile ( folderPath + "/dragons.html" );
 } );
 
-app.listen( 8080, ( error ) => {
+app.listen( PORT, ( error ) => {
     if ( error ) {
         console.log( error );
     }
-    console.log( "Server running on port: ", 8080 );
+    console.log( "Server running on port: ", Number( PORT ) );
+    // $env:PORT="3000"; nodemon app.js - POWERSHELL
+    // set PORT=3000 & nodemon app.js - CMD
 } );
