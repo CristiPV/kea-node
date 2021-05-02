3ce0b1b83fbf8d531165965a7c943b60b458a510
Cristian <cristi.valentin29@gmail.com>
2021-05-02 10:41:52 +0200

    Nodefolio: add projects data

--- 8._Nodefolio/routes/projects.js
+++ 8._Nodefolio/routes/projects.js
@@ -4,23 +4,23 @@ const projects = [
     {
         title: "Nodefolio",
         description: "Description - To be added",
-        gitURl: "GitHub URL - To be added",
-        images: [],
-        technologiesInvolved: ["Node.js", "HTML", "CSS"]
+        gitURL: "https://github.com/CristiPV/kea-node/tree/main/8._Nodefolio",
+        image: "project-icon-nodefolio.svg",
+        technologiesInvolved: ["Node.js", "HTML", "CSS", "Bootstrap"]
     },
     {
         title: "Treecreate",
         description: "Description - To be added",
-        gitURl: "GitHub URL - To be added",
-        images: [],
-        technologiesInvolved: []
+        gitURL: "https://github.com/treecreate",
+        image: "project-icon-treecreate.svg",
+        technologiesInvolved: ["Angular", "Java", "Springboot", "Typescript", "HTML", "CSS"]
     },
     {
         title: "KEA Node Mandatory I",
         description: "Description - To be added",
-        gitURl: "GitHub URL - To be added",
-        images: [],
-        technologiesInvolved: []
+        gitURL: "https://github.com/CristiPV/kea-node-mandatory-I",
+        image: "project-icon-mandatory.svg",
+        technologiesInvolved: ["Node.js", "HTML", "CSS", "Bootstrap"]
     }
 ];
 
